045251fb407a6e8462e3793c946deccbc7073996
Agata Jackiewicz <ag.jackiewicz@gmail.com>
2022-03-27 18:30:28 +0200

    empty list info

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -2,14 +2,24 @@ import styles from './UserList.module.scss';
 import Card from '../ui/Card';
 import { useSelector, useDispatch } from 'react-redux';
 import { userListActions } from '../../store/userList-slice';
-import UserListItem from './UserListItem'
+import UserListItem from './UserListItem';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 const UserList = () => {
 
 	const users = useSelector(state => state.usersList.users);
 
+
 	return (
 		<Card>
+			{(users.length === 0) ? <div className={styles.empty}>
+				<p>There is currently no users.</p>
+				<p>To add user to list first Generate New User and then click "Add User" Icon &rarr; <FontAwesomeIcon icon={faUserPlus} />
+				</p>
+				<p>You can store maximum 10 users. If you add eleveth user first one will be removed from list.</p>
+
+			</div> :
 			<div className={styles.users}>
 		        <table className={styles.users__list}>
 		         <thead className={styles.users__header}>
@@ -32,7 +42,7 @@ const UserList = () => {
 			          />
 			       ))}     
 		        </tbody></table>
-			</div>
+			</div>}
 		</Card>
 	)
 }
